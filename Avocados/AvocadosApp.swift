3cd005597cb3c3f6cda5bfe44e28d121b371474f
Itunu Raimi <chavitunu@gmail.com>
2021-02-22 16:02:16 +0100

    Initial Commit

---
+++ Avocados/AvocadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosApp.swift
+//  Avocados
+//
+//  Created by Itunu Raimi on 22/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
