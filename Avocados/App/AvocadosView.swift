82731ccfb9510a867eaa5477e52a231853e1b1d9
Itunu Raimi <chavitunu@gmail.com>
2021-02-23 08:56:53 +0100

    added pulsation animations to the Avocados Page

--- Avocados/App/AvocadosView.swift
+++ Avocados/App/AvocadosView.swift
@@ -8,13 +8,59 @@
 import SwiftUI
 
 struct AvocadosView: View {
+    // MARK: - PROPERTIES
+    @State private var pulsateAnimation = false
+    
+    // MARK: - BODY
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .center, spacing: 10, content: {
+            Spacer()
+            Image("avocado")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 240, height: 240, alignment: .center)
+                .shadow(color: Color("ColorBlackTransparentDark"), radius: 12, x: 0, y: 8)
+                .scaleEffect(pulsateAnimation ? 1 : 0.9)
+                .opacity(pulsateAnimation ? 1 : 0.9)
+                .animation(Animation.easeInOut(duration: 1.5).repeatForever(autoreverses: true))
+            VStack {
+                Text("Avocados".uppercased())
+                    .foregroundColor(.white)
+                    .font(.system(size: 42, weight: .bold, design: .serif))
+                    .fontWeight(.heavy)
+                    .padding()
+                    .shadow(color: Color("ColorBlackTransparentDark"), radius: 4, x: 0, y: 4)
+                
+                Text("""
+Creamy, green, and full of nutrients!
+Avocado is a powerhouse ingredient at any meal. Enjoy these handpicked avocado recipes for breakfast, lunch, dinner & more!
+""")
+                    .lineLimit(nil)
+                    .font(.system(.headline, design: .serif))
+                    .foregroundColor(Color("ColorGreenLight"))
+                    .multilineTextAlignment(.center)
+                    .lineSpacing(8)
+                    .frame(maxWidth: 640, minHeight: 120)
+            } //: VSTACK
+            .padding()
+            
+            Spacer()
+        }) //: VSTACK
+        .background(
+            Image("background")
+                .resizable()
+                .scaledToFill()
+        )
+        .edgesIgnoringSafeArea(.all)
+        .onAppear(){
+            pulsateAnimation.toggle()
+        }
     }
 }
 
 struct AvocadosView_Previews: PreviewProvider {
     static var previews: some View {
         AvocadosView()
+            
     }
 }
