183aceb00dc429b9c059c3b8829fd2ff0defa7d0
Itunu Raimi <chavitunu@gmail.com>
2021-02-24 13:49:52 +0100

    designed settings screen

--- Avocados/App/SettingsView.swift
+++ Avocados/App/SettingsView.swift
@@ -8,8 +8,94 @@
 import SwiftUI
 
 struct SettingsView: View {
+    // MARK: - PROPERTIES
+    @State private var enableNotifications = true
+    @State private var backgroundRefresh = false
+    
+    // MARK: - BODY
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .center, spacing: 0) {
+            // MARK: - HEADER
+            
+            VStack(alignment: .center, spacing: 5) {
+                Image("avocado")
+                    .resizable()
+                    .scaledToFit()
+                    .padding(.top)
+                    .frame(width: 100, height: 100, alignment: .center )
+                    .shadow(color: Color("ColorBlackTransparentLight"), radius: 8, x: 0, y: 4)
+                
+                Text("Avocados".uppercased())
+                    .font(.system(.title, design: .serif))
+                    .fontWeight(.bold)
+                    .foregroundColor(Color("ColorGreenAdaptive"))
+            } //: VSTACK
+            .padding()
+            
+            Form {
+                // MARK: - SECTION 1
+                Section(header: Text("General Settings")) {
+                    Toggle(isOn: $enableNotifications, label: {
+                        Text("Enable notification")
+                    })
+                    
+                    Toggle(isOn: $backgroundRefresh, label: {
+                        Text("Background refresh")
+                    })
+                }
+                
+                // MARK: - SECTION 2
+                Section(header: Text("Application")) {
+                    if enableNotifications {
+                        HStack {
+                            Text("Product").foregroundColor(.gray)
+                            Spacer()
+                            Text("Avocado Recipes")
+                        }
+                        
+                        HStack {
+                            Text("Compatibility").foregroundColor(.gray)
+                            Spacer()
+                            Text("iPhone & iPad ")
+                        } //: HSTACK
+                        
+                        HStack {
+                            Text("Developer").foregroundColor(.gray)
+                            Spacer()
+                            Text("Itunu Raimi")
+                        } //: HSTACK
+                        
+                        HStack {
+                            Text("Designer").foregroundColor(.gray)
+                            Spacer()
+                            Text("Robert Petras")
+                        } //: HSTACK
+                        
+                        HStack {
+                            Text("Website").foregroundColor(.gray)
+                            Spacer()
+                            Text("swiftuimasterclass.com")
+                        } //: HSTACK
+                        
+                        HStack {
+                            Text("Version").foregroundColor(.gray)
+                            Spacer()
+                            Text("1.0.0 ")
+                        } //: HSTACK
+                    } else {
+                        HStack {
+                            Text("Personal message").foregroundColor(.gray)
+                            Spacer()
+                            Text("Enjoy the app!")
+                        }
+                    } //: HSTACK
+                    
+                    
+                }
+                
+            } //: FORM
+        } //: VSTACK
+        .frame(maxWidth: 640)
     }
 }
 
